3b684e6e1c89286c0847ebf3a8a667a7032f0406
Imran <imranali74@hotmail.com>
2018-08-27 19:55:06 +1000

    first commit

---
+++ JenkinCA/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinCA
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static string GetMessage()
+        {
+            return "Hello World";
+        }
+    }
+}
---
+++ TestJenkinCA/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JenkinCA;
+namespace TestJenkinCA
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.Equals("Hello World", Program.GetMessage());
+        }
+    }
+}
